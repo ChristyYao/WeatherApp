3372cf85ceb29482c6446496a5cab0365aa41ed9
christyyao <yao.christy@gmail.com>
2019-03-06 12:10:56 +0100

    curr city

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 
 $.getJSON("https://api.ipdata.co/?api-key=70d2666caac0a3efd237327444448c0c31493cd57ed499ff76c355f7", function(data){
   console.log(data);
-  var city = data.time_zone.name.split('/')[1];
+  // var city = data.time_zone.name.split('/')[1];
+  var city = data.city;
   // console.log(city);
 
   $('.city').append("You're in " + city + " your current weather is: ");
-});
 
-var city = "Amsterdam";
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=d657bc5415c2c79bd49a746b4e200023",
-function(data){
-  console.log(data);
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=d657bc5415c2c79bd49a746b4e200023",
+  function(data){
+    console.log(data);
 
-  var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 
-  var temp = Math.floor(data.main.temp);
+    var temp = Math.floor(data.main.temp);
 
-  var weather = data.weather[0].main;
+    var weather = data.weather[0].main;
 
-  // console.log(icon);
-  $('.icon').attr('src', icon);
-  $('.weather').append(weather);
-  $('.temp').append(temp + ' Celsius');
+    // console.log(icon);
+    $('.icon').attr('src', icon);
+    $('.weather').append(weather);
+    $('.temp').append(temp + ' Celsius');
+  });
+
 });

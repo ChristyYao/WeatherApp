5d6d3ea6eb1a7b50c3440cd52bbda65da1da80a2
christyyao <yao.christy@gmail.com>
2019-03-10 20:31:19 +0100

    current location weather and user input city weather

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 
+//get current location and display current weather
+//doens't work for "Ballerup Municipality"
+
+$.getJSON("https://api.ipdata.co/?api-key=70d2666caac0a3efd237327444448c0c31493cd57ed499ff76c355f7", function(data){
+  console.log(data);
+  // var city = data.time_zone.name.split('/')[1];
+  var city = data.city;
+  // console.log(city);
+
+  document.getElementById("currCity").innerHTML = "You're in " + city + " your current weather is: ";
+
+
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=d657bc5415c2c79bd49a746b4e200023",
+  function(data){
+    console.log(data);
+
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+    var temp = Math.floor(data.main.temp);
+
+    var weather = data.weather[0].main;
+
+    // console.log(icon);
+    $('#currIcon').attr('src', icon);
+    document.getElementById("currWeather").innerHTML = weather;
+    document.getElementById("currTemp").innerHTML = temp + " Celsius";
+  });
+
+});
+
+
+
+
 var api = "http://api.openweathermap.org/data/2.5/weather?q=";
 // var city = "Copenhagen";
 var units = "&units=metric";
@@ -21,44 +54,18 @@ function getWeather(){
 
     // console.log(icon);
     $('.icon').attr('src', icon);
-    $('.weather').append(weather);
-    $('.temp').append(temp + ' Celsius');
+    // $('.weather').append(weather);
+    // $('.temp').append(temp + ' Celsius');
+    document.getElementById("weather").innerHTML = weather;
+    document.getElementById("temp").innerHTML = temp + " Celsius";
   })
 }
 
 
 
 
-//get current location and display current weather
-//doens't work for "Ballerup Municipality"
-/*
-$.getJSON("https://api.ipdata.co/?api-key=70d2666caac0a3efd237327444448c0c31493cd57ed499ff76c355f7", function(data){
-  console.log(data);
-  // var city = data.time_zone.name.split('/')[1];
-  var city = data.city;
-  // console.log(city);
-
-  $('.city').append("You're in " + city + " your current weather is: ");
 
 
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=d657bc5415c2c79bd49a746b4e200023",
-  function(data){
-    console.log(data);
-
-    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-
-    var temp = Math.floor(data.main.temp);
-
-    var weather = data.weather[0].main;
-
-    // console.log(icon);
-    $('.icon').attr('src', icon);
-    $('.weather').append(weather);
-    $('.temp').append(temp + ' Celsius');
-  });
-
-});
-*/
 
 
 

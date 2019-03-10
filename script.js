fcebb29d8e3aa665e289bf1db84a0a10a1b1051b
christyyao <yao.christy@gmail.com>
2019-03-10 20:01:16 +0100

    manual input but doesn't overwrite previous displayed data

--- script.js
+++ script.js
@@ -1,34 +1,74 @@
 
-// $.getJSON("https://api.ipdata.co/?api-key=70d2666caac0a3efd237327444448c0c31493cd57ed499ff76c355f7", function(data){
-//   console.log(data);
-//   // var city = data.time_zone.name.split('/')[1];
-//   var city = data.city;
-//   // console.log(city);
-//
-//   $('.city').append("You're in " + city + " your current weather is: ");
-//
-//
-//   $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=d657bc5415c2c79bd49a746b4e200023",
-//   function(data){
-//     console.log(data);
-//
-//     var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-//
-//     var temp = Math.floor(data.main.temp);
-//
-//     var weather = data.weather[0].main;
-//
-//     // console.log(icon);
-//     $('.icon').attr('src', icon);
-//     $('.weather').append(weather);
-//     $('.temp').append(temp + ' Celsius');
-//   });
-//
-// });
+var api = "http://api.openweathermap.org/data/2.5/weather?q=";
+// var city = "Copenhagen";
+var units = "&units=metric";
+var apiKey = "&APPID=d657bc5415c2c79bd49a746b4e200023";
+
+function hi(){
+  console.log("hi");
+}
+
+function getWeather(){
+  var city = document.getElementById("city").value;
+  var url = api + city + units + apiKey;
+
+  $.getJSON(url, function(data){
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+    var temp = Math.floor(data.main.temp);
+
+    var weather = data.weather[0].main;
+
+    // console.log(icon);
+    $('.icon').attr('src', icon);
+    $('.weather').append(weather);
+    $('.temp').append(temp + ' Celsius');
+  })
+}
 
 
 
 
+//get current location and display current weather
+//doens't work for "Ballerup Municipality"
+/*
+$.getJSON("https://api.ipdata.co/?api-key=70d2666caac0a3efd237327444448c0c31493cd57ed499ff76c355f7", function(data){
+  console.log(data);
+  // var city = data.time_zone.name.split('/')[1];
+  var city = data.city;
+  // console.log(city);
+
+  $('.city').append("You're in " + city + " your current weather is: ");
+
+
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=d657bc5415c2c79bd49a746b4e200023",
+  function(data){
+    console.log(data);
+
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+    var temp = Math.floor(data.main.temp);
+
+    var weather = data.weather[0].main;
+
+    // console.log(icon);
+    $('.icon').attr('src', icon);
+    $('.weather').append(weather);
+    $('.temp').append(temp + ' Celsius');
+  });
+
+});
+*/
+
+
+
+
+
+
+
+// trying to make current location's weather work
+//(sometimes the currenty location has a different name than what is in the weather database)
+/*
 
 // var city = null;
 
@@ -70,3 +110,5 @@ function weather_data(city){
 
   // });
 }
+
+*/
